a0e25914fb2f1a6a9f1b29a2cc4a3625b6cdb697
Jens Ettl <ettljens@gmail.com>
2023-10-02 22:48:26 +0200

    draw_grid

--- src/main.py
+++ src/main.py
@@ -8,13 +8,46 @@ TILE_SIZE = 20
 GRID_WIDTH, GRID_HEIGHT = WIDTH / TILE_SIZE, HEIGHT / TILE_SIZE
 FPS = 60
 
-# Create the screen
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (50, 50, 50)
 YELLOW = (255, 255, 0)
 
+# Create the screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+
+# draw_grid
+def draw_grid():
+    for x in range(0, WIDTH, TILE_SIZE):  # 0, 20, 40, 60, ..., 780
+        pygame.draw.line(screen, GREY, (x, 0), (x, HEIGHT))
+
+    for y in range(0, HEIGHT, TILE_SIZE):
+        pygame.draw.line(screen, GREY, (0, y), (WIDTH, y))
+
+
+# Main loop
+def main():
+    running = True
+
+    while running:
+        clock.tick(FPS)
+
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Update
+        # Draw
+        screen.fill(BLACK)
+        draw_grid()
+        pygame.display.update()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
