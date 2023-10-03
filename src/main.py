622c85ca912125bb8445840af6b3f60067d470e9
Jens Ettl <ettljens@gmail.com>
2023-10-03 23:05:21 +0200

    added def adjust_position and def get_neighbours

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ pygame.init()
 WIDTH, HEIGHT = 800, 800
 TILE_SIZE = 20
 GRID_WIDTH, GRID_HEIGHT = WIDTH / TILE_SIZE, HEIGHT / TILE_SIZE
-FPS = 60
+FPS = 10
 
 # Colors
 BLACK = (0, 0, 0)
@@ -21,7 +21,7 @@ clock = pygame.time.Clock()
 
 
 # generate_random_positions
-def generate_random_positions(n):
+def generate_random_positions(n: int):
     positions = set()
     for _ in range(n):
         x = random.randrange(GRID_WIDTH)
@@ -31,7 +31,7 @@ def generate_random_positions(n):
 
 
 # draw_grid
-def draw_grid(positions):
+def draw_grid(positions: set):
     for position in positions:
         x, y = position
         pygame.draw.rect(
@@ -45,6 +45,35 @@ def draw_grid(positions):
         pygame.draw.line(screen, GREY, (0, y), (WIDTH, y))
 
 
+# adjust_positions - Conway's Game of Life
+def adjust_positions(positions: set):
+    new_positions = set()
+    for position in positions:
+        neighbours = get_neighbours(position)
+        alive_neighbours = len(positions.intersection(neighbours))
+        if alive_neighbours == 2 or alive_neighbours == 3:
+            new_positions.add(position)
+
+        for neighbour in neighbours:
+            neighbours_neighbours = get_neighbours(neighbour)
+            alive_neighbours = len(positions.intersection(neighbours_neighbours))
+            if alive_neighbours == 3:
+                new_positions.add(neighbour)
+    return new_positions
+
+
+# get_neighbours - helper function for adjust_positions
+def get_neighbours(pos: tuple):
+    x, y = pos
+    neighbours = set()
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            neighbour = (x + dx, y + dy)
+            if neighbour != pos:
+                neighbours.add(neighbour)
+    return neighbours
+
+
 # Main loop
 def main():
     running = True
@@ -52,6 +81,10 @@ def main():
     positions = set()
 
     while running:
+        # Handle time
+        if playing:
+            positions = adjust_positions(positions)
+
         clock.tick(FPS)
 
         # Handle events
@@ -82,6 +115,9 @@ def main():
         # Update
         # Draw
         screen.fill(BLACK)
+        pygame.display.set_caption(
+            "Game of Life - Playing" if playing else "Game of Life - Paused"
+        )
         draw_grid(positions)
         pygame.display.update()
 
